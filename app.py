3116690e3e4d2249c60218ef93920992788eff9c
qbouet <quentin.bouet@my.jcu.edu.au>
2023-11-23 12:48:58 +1000

    Add docstrings to Flask Project

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+"""
+CP1404 Prac 10 Flask Project
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,22 +9,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """Normal function that prints hello world"""
     return "<h1>Hello World :)</h1>"
 
 
 @app.route('/greet')
 @app.route('/greet/<name>')
 def greet(name=""):
+    """Greet person using their name"""
     return f"Hello {name}"
 
 
 @app.route('/f')
 @app.route('/f/<celsius>')
 def f(celsius=""):
+    """Convert fahrenheit to celsius"""
     return str(convert_celsius_to_fahrenheit(float(celsius)))
 
 
 def convert_celsius_to_fahrenheit(celsius):
+    """Convert fahrenheit to celsius"""
     return celsius * 9.0 / 5 + 32
 
 
